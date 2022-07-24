e5c49920b7efd1943936c7b5e6e1f07c4d7fe733
demostanis <demostanis@protonmail.com>
2022-07-24 12:57:32 +0200

    Userland: Make uniq(1) POSIX
    Adds -d, -c, -i, -f and -s options.
    It differs a little bit from other implementations,
    but it does the trick and corresponds to POSIX's description

--- Userland/Utilities/uniq.cpp
+++ Userland/Utilities/uniq.cpp
@@ -5,75 +5,127 @@
  */
 
 #include <AK/RefPtr.h>
+#include <AK/StringView.h>
 #include <LibCore/ArgsParser.h>
+#include <LibCore/Stream.h>
 #include <LibCore/System.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
-struct linebuf {
-    char* buf = NULL;
-    size_t len = 0;
-};
-
-static FILE* get_stream(char const* filepath, char const* perms)
+static Optional<String> get_line_content(StringView line, size_t count, bool show_duplicates, bool print_count)
 {
-    FILE* ret;
-
-    if (filepath == nullptr) {
-        if (perms[0] == 'r')
-            return stdin;
-        return stdout;
+    if (!show_duplicates || count > 0) {
+        if (print_count)
+            return String::formatted("{} {}\n", count + 1, line);
+        else
+            return String::formatted("{}\n", line);
     }
+    return {};
+}
 
-    ret = fopen(filepath, perms);
-    if (ret == nullptr) {
-        perror("fopen");
-        exit(1);
+static StringView skip(StringView buf, unsigned int nchars, unsigned int nfields)
+{
+    StringView tmp(buf);
+    tmp = tmp.trim("\n"sv);
+
+    if (nfields) {
+        bool in_field = false;
+        auto fields = tmp.find_all([&](char c) {
+            if (c == ' ' || c == '\t' || c == '\v' || c == '\f' || c == '\r')
+                in_field = false;
+            else if (!in_field) {
+                in_field = true;
+                return true;
+            }
+            return false;
+        });
+        if (nfields >= fields.size())
+            nfields = fields.size() - 1;
+        tmp = tmp.substring_view(fields[nfields]);
     }
+    if (tmp.length() < nchars)
+        nchars = tmp.length();
 
-    return ret;
+    tmp = tmp.substring_view(nchars);
+    return tmp;
+}
+
+// Borrowed from cmp.cpp, there should probably be a simpler way
+static ErrorOr<NonnullOwnPtr<Core::Stream::BufferedFile>> open_file_or_stdin(String const& filename)
+{
+    OwnPtr<Core::Stream::File> file;
+    if (filename.is_empty() || filename == "-")
+        file = TRY(Core::Stream::File::adopt_fd(STDIN_FILENO, Core::Stream::OpenMode::Read));
+    else
+        file = TRY(Core::Stream::File::open(filename, Core::Stream::OpenMode::Read));
+    return TRY(Core::Stream::BufferedFile::create(file.release_nonnull()));
 }
 
 ErrorOr<int> serenity_main(Main::Arguments arguments)
 {
     TRY(Core::System::pledge("stdio rpath wpath cpath"));
 
-    char const* inpath = nullptr;
-    char const* outpath = nullptr;
+    StringView inpath;
+    StringView outpath;
+    bool duplicates = false;
+    bool unique = false;
+    bool ignore_case = false;
+    bool print_count = false;
+    unsigned int skip_chars = 0;
+    unsigned int skip_fields = 0;
+
     Core::ArgsParser args_parser;
+    args_parser.add_option(duplicates, "Only print duplicated lines", "repeated", 'd');
+    args_parser.add_option(unique, "Only print unique lines (default)", "unique", 'u');
+    args_parser.add_option(ignore_case, "Ignore case when comparing lines", "ignore-case", 'i');
+    args_parser.add_option(print_count, "Prefix each line by its number of occurences", "count", 'c');
+    args_parser.add_option(skip_chars, "Skip N chars", "skip-chars", 's', "N");
+    args_parser.add_option(skip_fields, "Skip N fields", "skip-fields", 'f', "N");
     args_parser.add_positional_argument(inpath, "Input file", "input", Core::ArgsParser::Required::No);
     args_parser.add_positional_argument(outpath, "Output file", "output", Core::ArgsParser::Required::No);
     args_parser.parse(arguments);
 
-    FILE* infile = get_stream(inpath, "r");
-    FILE* outfile = get_stream(outpath, "w");
+    if (!unique && !duplicates)
+        unique = true;
+    else if (unique && duplicates)
+        // Printing duplicated and unique lines shouldn't print anything
+        exit(0);
 
-    struct linebuf buffers[2];
-    struct linebuf* previous = &(buffers[0]);
-    struct linebuf* current = &(buffers[1]);
-    bool first_run = true;
-    for (;;) {
-        errno = 0;
-        ssize_t rc = getline(&(current->buf), &(current->len), infile);
-        if (rc < 0 && errno != 0) {
-            perror("getline");
-            exit(1);
-        }
-        if (rc < 0)
-            break;
-        if (!first_run && strcmp(current->buf, previous->buf) == 0)
-            continue;
+    if (outpath.is_empty() || outpath == "-"sv)
+        outpath = "/dev/stdout"sv;
 
-        fputs(current->buf, outfile);
+    auto infile = TRY(open_file_or_stdin(inpath));
+    auto outfile = TRY(Core::Stream::File::open(outpath, Core::Stream::OpenMode::Write));
+
+    size_t count = 0;
+    StringView current;
+    ByteBuffer previous_buf = TRY(ByteBuffer::create_zeroed(1024));
+    ByteBuffer current_buf = TRY(ByteBuffer::create_zeroed(1024));
+
+    StringView previous = TRY(infile->read_line(previous_buf));
+    StringView previous_to_compare = skip(previous, skip_chars, skip_fields);
+
+    while (TRY(infile->can_read_line())) {
+        current = TRY(infile->read_line(current_buf));
+
+        StringView current_to_compare = skip(current, skip_chars, skip_fields);
+        if (ignore_case ? (!current_to_compare.equals_ignoring_case(previous_to_compare)) : (current_to_compare != previous_to_compare)) {
+            auto maybe_line_content = get_line_content(previous, count, duplicates, print_count);
+            if (maybe_line_content.has_value())
+                TRY(outfile->write(maybe_line_content.value().bytes()));
+            count = 0;
+        } else
+            count++;
+        swap(current_to_compare, previous_to_compare);
+        swap(current_buf, previous_buf);
         swap(current, previous);
-        first_run = false;
     }
 
-    fclose(infile);
-    fclose(outfile);
+    auto maybe_line_content = get_line_content(previous, count, duplicates, print_count);
+    if (maybe_line_content.has_value())
+        TRY(outfile->write(maybe_line_content.value().bytes()));
+
+    infile->close();
+    outfile->close();
 
     return 0;
 }
